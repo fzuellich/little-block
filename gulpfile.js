3dca502cb7b11aa8c97371321c4c42943c07c138
fzuellich <fzuellich@gmail.com>
2016-01-23 20:06:18 +0100

    Added Gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,28 @@
 var gulp = require('gulp');
+var del = require('del');
+var requirejsOptimize = require('gulp-requirejs-optimize');
+var bower = require('main-bower-files');
 
-gulp.task('default', function() {
-    // hello world
+requireJS_config = {
+    baseUrl: 'src/js',
+    name: 'app',
+    out: 'app.js'
+};
+
+gulp.task('clean:dist', function() {
+    return del(['dist']);
 });
+
+gulp.task('move:dependencies', ['clean:dist'], function() {
+    return gulp.src(bower()).pipe(gulp.dest('dist/js/extern'));
+});
+
+gulp.task('make:dev', ['move:dependencies'], function() {
+    gulp.src('src/index.html').pipe(gulp.dest('dist'));
+
+    gulp.src('src/js/app.js')
+        .pipe(requirejsOptimize({optimize: 'none'}))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('build', ['make:dev']);
